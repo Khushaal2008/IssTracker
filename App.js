5383e26c21951f6c4d73b99109de9b185f21691e
Khushaal2008 <75684230+Khushaal2008@users.noreply.github.com>
2021-08-12 21:04:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import IssLocationScreen from './screens/IssLocationScreen';
+import MeteorScreen from './screens/MeteorScreen';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack =createStackNavigator();
+
+  function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator  initialRouteName = "HomeScreen" screenOptions={{
+        headerShown :false
+      }} >
+        <Stack.Screen name = "HomeScreen" component= {HomeScreen}/>
+        <Stack.Screen name = "IssLocationScreen" component={IssLocationScreen}/>
+        <Stack.Screen name = "MeteorScreen" component={MeteorScreen}/>
+
+        </Stack.Navigator>
+    </NavigationContainer>
+    
+  );
+}
+ export default App;
